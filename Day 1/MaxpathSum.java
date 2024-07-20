d20a05fa1c089477110ec98042ee8b040b1017e0
Aaruu008 <thakuraryanchauhan008@gmail.com>
2024-07-20 16:54:04 +0530

    edited

--- Day 1/MaxpathSum.java
+++ Day 1/MaxpathSum.java
@@ -1,4 +1,24 @@
 
+/*class Node {
+    int data;
+    Node left;
+    Node right;
+    Node() {
+        this.data = 0;
+        this.left = null;
+        this.right = null;
+    }
+    Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+    Node(int data, Node left, Node right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+};*/
 
 public class MaxpathSum {
     public int maxPathSum(Node root) {
